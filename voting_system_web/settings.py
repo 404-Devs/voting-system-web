cb218e1b38f9a47e8d0062e1b314b60cdcf714a4
Stephen Mwangi <st3v3n.mw@gmail.com>
2020-04-14 20:46:20 +0300

    Heroku Config

--- voting_system_web/settings.py
+++ voting_system_web/settings.py
@@ -123,3 +123,6 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
